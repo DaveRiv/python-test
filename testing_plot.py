df7524027d6d768ce523665fb918f96940e405d0
Sholem <sholem@Elazar-Rivkins-MacBook-Pro.local>
2015-05-28 16:03:15 -0400

    files added to gitHub

---
+++ testing_plot.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+import matplotlib as mpl
+
+x_range = np.arange(0.1,10,0.01)
+
+y_range = np.sin(x_range)
+
+np.sin(x_range)
+
+from numpy import*
+
+import matplotlib.pyplot as plt
+
+plt.plot(x_range, y_range, '--,r')
+
+plt.savefig('plot_2')
